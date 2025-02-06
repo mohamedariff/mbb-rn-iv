ec9052b38fe4d05ac32f81cdca879d6d6dc8376c
Ariff Rahim <mohamedariff.abdrahim@gmail.com>
2025-02-06 22:14:27 +0800

    add list with fake data

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,17 +1,54 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native'
 import { Input } from '@ant-design/react-native'
 
+type ItemProps = { title: string }
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item'
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item'
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item'
+  }
+]
+
 function CustomInput() {
+  const Item = ({ title }: ItemProps) => (
+    <TouchableOpacity>
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  )
+
   return (
-    <Input
-      allowClear
-      type="text"
-      autoComplete="off"
-      style={styles.input}
-      inputStyle={styles.innerInput}
-      placeholder="Search a place.."
-    />
+    <View style={{ gap: 10 }}>
+      <Input
+        allowClear
+        type="text"
+        autoComplete="off"
+        style={styles.input}
+        inputStyle={styles.innerInput}
+        placeholder="Search a place.."
+      />
+      <FlatList
+        style={styles.list}
+        data={DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Item title={item.title} />}
+      />
+    </View>
   )
 }
 
@@ -27,6 +64,15 @@ const styles = StyleSheet.create({
   },
   innerInput: {
     padding: 10
+  },
+  list: {
+    width: '90%',
+    maxHeight: 400,
+    borderRadius: 5,
+    alignSelf: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    backgroundColor: 'white'
   }
 })
 
