a542049d3c2e3b3db268f4f029ebe5009617beb9
Ariff Rahim <mohamedariff.abdrahim@gmail.com>
2025-02-07 00:24:48 +0800

    improve hoc

--- hoc/withApiKeyCheck.tsx
+++ hoc/withApiKeyCheck.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect } from 'react'
 import { Toast } from '@ant-design/react-native'
+import { isUndefined } from 'lodash'
 
 export const withApiKeyCheck = (WrappedComponent: React.ComponentType<any>) => {
   return (props: any) => {
     useEffect(() => {
-      console.log(
-        '=======process.env.EXPO_PUBLIC_PLACES_KEY:',
-        process.env.EXPO_PUBLIC_PLACES_KEY
-      )
-      if (!!process.env.EXPO_PUBLIC_PLACES_KEY) {
+      if (isUndefined(process.env.EXPO_PUBLIC_PLACES_KEY)) {
         Toast.fail(
           'Google Places API key is missing. I have include the key in email. Please ask Martina Ridzuan from the HR'
         )
